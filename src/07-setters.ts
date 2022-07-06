2585a523f8247dc1b8e5274f60fca5a341ebb390
Oscar Chapa <tutoschapa@gmail.com>
2022-07-06 11:22:25 -0600

    Clase de setters

--- src/07-setters.ts
+++ src/07-setters.ts
@@ -38,10 +38,21 @@ export class MyDate {
       (this.year % 400 === 0 || this.year % 100 !== 0) && this.year % 4 == 0
     );
   }
-
   // Todos los getters necesitan retornal algun valor, y no tienen parametros.
+
+  set month(newValue) {
+    if (newValue >= 0 && newValue <= 11) {
+      this._month = newValue;
+    } else {
+      throw new Error('ERROR: Month out of range.')
+    }
+  }
+  // Los setters son utiles si queremos que un atributo necesite tener reglas
+  // En caso contrario si el atributo puede tener valor sin rango, mejor lo dejamos public.
 }
 
 const myDate = new MyDate(2022, 2, 3);
 myDate.day; // .day hace referencia al getter de day.
 myDate.isLeapYear // Aqui tenemos un nuevo atributo que nos indica si el año es bisiesto
+myDate.month = 2;
+myDate.month = 12; // Error
